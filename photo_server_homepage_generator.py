d0d0d2966a9fbab733924d3eef853fdfd51a6e51
Drew Maatman <dmaatman@xes-inc.com>
2025-08-05 12:52:14 -0500

    added loop in template html

--- photo_server_homepage_generator.py
+++ photo_server_homepage_generator.py
@@ -1,8 +1,23 @@
 
+from jinja2 import Environment, FileSystemLoader
+
+data = {"items": ["apple", "banana", "cherry"]}
+
 
 def main():
 
-    print("Hello, world!")
+    # Set up the Jinja2 environment to load templates from the current directory
+    env = Environment(loader=FileSystemLoader('./templates/'))
+
+    # Load the template
+    template = env.get_template('template.html')
+
+    # render data in jinja template
+    demoText = template.render(data)
+
+    # print(inviteText)
+    with open(f"index.html", mode='w') as f:
+        f.write(demoText)
 
 
 if __name__== "__main__":
