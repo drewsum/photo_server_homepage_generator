cef467a7216b6e7943a257220c38ba3a6553165a
Drew Maatman <drewmaatman@gmail.com>
2025-08-06 06:50:59 -0500

    added real data requests

--- photo_server_homepage_generator.py
+++ photo_server_homepage_generator.py
@@ -18,6 +18,16 @@ data = [
     }
 ]
 
+def get_all_shared_links(api_key, url):
+
+    payload = {}
+    headers = {
+        'Accept': 'application/json',
+        'x-api-key': api_key
+    }
+    response = requests.request("GET", url+"/api/shared-links", headers=headers, data=payload)
+    
+    return response.json()
 
 def main():
 
@@ -32,6 +42,11 @@ def main():
         print("Found Immich API env variable: {}".format(immich_api_key))
     else:
         print("Could not find IMMICH_API_KEY environment variable, please add it!")
+
+    # get all shared links from immich server
+    shared_links_list = get_all_shared_links(immich_api_key, immich_server_url)
+    for link in shared_links_list:
+        print(f"found shared link id: {link['id']}")
 
     # Set up the Jinja2 environment to load templates from the current directory
     env = Environment(loader=FileSystemLoader('./templates/'))
